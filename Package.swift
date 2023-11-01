a2f9fa2d2450d0246e7cda5ea88cfec083515317
metalurgical <97008724+metalurgical@users.noreply.github.com>
2023-11-01 14:37:24 +0200

    feat: update torusutils version 6.1.0

--- Package.swift
+++ Package.swift
@@ -9,23 +9,18 @@ let package = Package(
         .iOS(.v14), .macOS(.v10_15)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "ThresholdKey",
             targets: ["tkey-pkg"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-        .package(name: "TorusUtils", url: "https://github.com/torusresearch/torus-utils-swift" , from: "6.0.1"),
+        .package(name: "TorusUtils", url: "https://github.com/torusresearch/torus-utils-swift" , from: "6.1.0"),
         .package(name: "secp256k1", url: "https://github.com/GigaBitcoin/secp256k1.swift" , .exact("0.12.2")),
         // dev dependencies only
         .package(name:"CryptoSwift", url: "https://github.com/krzyzanowskim/CryptoSwift",from: "1.5.1"),
         .package(name:"jwt-kit", url: "https://github.com/vapor/jwt-kit", from: "4.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(name: "libtkey",
                       path: "Sources/libtkey/libtkey.xcframework"
         ),
@@ -35,7 +30,7 @@ let package = Package(
         ),
         .target(
             name: "tkey-pkg",
-            dependencies: ["lib", "TorusUtils"],
+            dependencies: ["lib", "TorusUtils", "secp256k1"],
             path: "Sources/ThresholdKey"
         ),
         .testTarget(
